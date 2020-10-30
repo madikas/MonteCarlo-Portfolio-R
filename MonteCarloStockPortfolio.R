1df694901d6ed4cd7411c2d114835edf93db5549
Madi Kas <kas-madius@gmail.com>
2020-10-31 03:51:44 +0600

    Stocks data collection for the last five years

--- MonteCarloStockPortfolio.R
+++ MonteCarloStockPortfolio.R
@@ -1,3 +1,14 @@
-" I just figured out how to make this work, Thank you! "
-" test"
-"test2"
+library(quantmod) 
+library(tidyverse)
+tickers=c("GOOGL", "AMZN", "PFE", "JPM", "CVX", "BA", "FAST", "INTC", 
+          "T", "NWSA")
+
+stocks <-lapply(tickers, function(x) {getSymbols(x, 
+                                      from = "2015/01/01", 
+                                      to = "2020/10/31",
+                                      periodicity = "daily",
+                                      auto.assign=FALSE)} )
+
+names(stocks) = tickers
+
+head(stocks$GOOGL)
