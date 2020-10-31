a7cf0ffaf3444ce3141ac8da05c45acca31048e1
Madi Kas <kas-madius@gmail.com>
2020-10-31 21:27:42 +0600

    Return Calculation and Efficient Frontier

--- MonteCarloStockPortfolio.R
+++ MonteCarloStockPortfolio.R
@@ -1,14 +1,42 @@
 library(quantmod) 
-library(tidyverse)
+library(timeSeries)
+library(fPortfolio)
+library(caTools)
+library(dplyr)
+library(PerformanceAnalytics)
+library(ggplot2)
+
+#Vector of Stock Tickers
 tickers=c("GOOGL", "AMZN", "PFE", "JPM", "CVX", "BA", "FAST", "INTC", 
           "T", "NWSA")
 
-stocks <-lapply(tickers, function(x) {getSymbols(x, 
-                                      from = "2015/01/01", 
-                                      to = "2020/10/31",
-                                      periodicity = "daily",
-                                      auto.assign=FALSE)} )
+#Daily Prices for the last five years
+stockPrices = NULL
+for (ticker in tickers)
+  stockPrices = cbind(stockPrices,
+                      getSymbols.yahoo(ticker, from="2015-01-01",
+                                       periodicity = "daily",
+                                        auto.assign=FALSE)[,4])
+#Keep dates that have closing prices for all tickers
+stockPrices = stockPrices[apply(stockPrices, 1, function(x) all(!is.na(x))),]
+#Rename Columns matching stock ticker names
+colnames(stockPrices) = tickers
 
-names(stocks) = tickers
+#Daily Rate of Change
+stockReturns = na.omit(ROC(stockPrices, type="discrete"))
+stockReturns = as.timeSeries(stockReturns)
 
-head(stocks$GOOGL)
+#efficient frontier
+effFrontier = portfolioFrontier(stockReturns, constraints = "LongOnly")
+
+#Plot efficient frontier
+#Plot options fportfolio
+#1 - Plot efficient frontier
+#2 - Plot Min Variance Portfolio
+#3 - Plot Tangency Portfolio
+#4 - Plot Risk Returns of each ticker
+#5 - Plot Equal Weights Portfolio
+#6 - Plot Two Asset Frontiers
+#7 - Plot Monte Carlo Portfolios
+#8 - Plot Sharpe Ratio
+plot(effFrontier, 1)
