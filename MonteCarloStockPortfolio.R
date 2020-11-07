223eefb529c0c831d0d83028afc671de64e46c36
Madi Kas <kas-madius@gmail.com>
2020-11-07 17:20:41 +0600

    Exponential distribution generation

--- MonteCarloStockPortfolio.R
+++ MonteCarloStockPortfolio.R
@@ -40,3 +40,54 @@ effFrontier = portfolioFrontier(stockReturns, constraints = "LongOnly")
 #7 - Plot Monte Carlo Portfolios
 #8 - Plot Sharpe Ratio
 plot(effFrontier, 1)
+
+# 1.Simulating Daily Returns for Individual Stock (Normal Distribution)
+
+#Parameters for the geometric mean & std dev of daily return for stock 
+m <- 0.003
+s <- 0.0098
+tradingdays<-253
+
+#Vector of daily returns
+
+set.seed(123)
+
+dailyreturns_norm <-rnorm(n=tradingdays,m,s)
+mean(dailyreturns_norm)
+sd(dailyreturns_norm)
+plot(density(dailyreturns_norm))
+
+# Generating Daily Price from Returns 
+STK_PRC <- 10
+r <-dailyreturns_norm
+stock_prices_norm <- c()
+for (i in seq(tradingdays))
+{
+  STK_PRC <- STK_PRC*(1+r[i])
+  stock_prices_norm <- c(stock_prices_norm,STK_PRC)
+  
+}
+print(stock_prices_norm)
+
+#2. Simulating Exponential Distribution for Daily Returns
+
+lambda = 30
+tradingdays = 253
+
+set.seed(123456)
+dailyreturns_exp = rexp(n=tradingdays, rate = lambda )
+mean(dailyreturns_exp)
+sd(dailyreturns_exp)
+hist(dailyreturns_exp)
+
+# Generating Daily Price from Returns 
+stockPrice <- 0.06
+r <-dailyreturns_exp
+stock_prices_exp <- c()
+for (i in seq(tradingdays))
+{
+  stockPrice <- stockPrice*(1+r[i])
+  stock_prices_exp <- c(stock_prices_exp,stockPrice)
+  
+}
+print(stock_prices_exp)
