732d7c6e0131a248cc4e31ddb69c8c1abc4a1e6a
Madi Kas <kas-madius@gmail.com>
2020-11-08 19:39:05 +0600

    simulation run scenarios

--- MonteCarloStockPortfolio.R
+++ MonteCarloStockPortfolio.R
@@ -1,93 +1,32 @@
-library(quantmod) 
+library(ggplot2)
+library(RiskPortfolios)
 library(timeSeries)
 library(fPortfolio)
 library(caTools)
 library(dplyr)
 library(PerformanceAnalytics)
-library(ggplot2)
+source("Simulation.R")
+#stock = scenario_stock_generation(253,"allnormal")
+#stock
 
-#Vector of Stock Tickers
-tickers=c("GOOGL", "AMZN", "PFE", "JPM", "CVX", "BA", "FAST", "INTC", 
-          "T", "NWSA")
-
-#Daily Prices for the last five years
-stockPrices = NULL
-for (ticker in tickers)
-  stockPrices = cbind(stockPrices,
-                      getSymbols.yahoo(ticker, from="2015-01-01",
-                                       periodicity = "daily",
-                                        auto.assign=FALSE)[,4])
-#Keep dates that have closing prices for all tickers
-stockPrices = stockPrices[apply(stockPrices, 1, function(x) all(!is.na(x))),]
-#Rename Columns matching stock ticker names
-colnames(stockPrices) = tickers
-
-#Daily Rate of Change
-stockReturns = na.omit(ROC(stockPrices, type="discrete"))
-stockReturns = as.timeSeries(stockReturns)
-
-#efficient frontier
-effFrontier = portfolioFrontier(stockReturns, constraints = "LongOnly")
-
-#Plot efficient frontier
-#Plot options fportfolio
-#1 - Plot efficient frontier
-#2 - Plot Min Variance Portfolio
-#3 - Plot Tangency Portfolio
-#4 - Plot Risk Returns of each ticker
-#5 - Plot Equal Weights Portfolio
-#6 - Plot Two Asset Frontiers
-#7 - Plot Monte Carlo Portfolios
-#8 - Plot Sharpe Ratio
-plot(effFrontier, 1)
-
-# 1.Simulating Daily Returns for Individual Stock (Normal Distribution)
-
-#Parameters for the geometric mean & std dev of daily return for stock 
-m <- 0.003
-s <- 0.0098
-tradingdays<-253
-
-#Vector of daily returns
-
-set.seed(123)
-
-dailyreturns_norm <-rnorm(n=tradingdays,m,s)
-mean(dailyreturns_norm)
-sd(dailyreturns_norm)
-plot(density(dailyreturns_norm))
-
-# Generating Daily Price from Returns 
-STK_PRC <- 10
-r <-dailyreturns_norm
-stock_prices_norm <- c()
-for (i in seq(tradingdays))
-{
-  STK_PRC <- STK_PRC*(1+r[i])
-  stock_prices_norm <- c(stock_prices_norm,STK_PRC)
-  
-}
-print(stock_prices_norm)
-
-#2. Simulating Exponential Distribution for Daily Returns
-
-lambda = 30
+#MonteCarlo simulation of portfolio strategies 
+#given different parameters(combination of distributions)
+simulations = 10
 tradingdays = 253
+scenario = "allnormal"
 
-set.seed(123456)
-dailyreturns_exp = rexp(n=tradingdays, rate = lambda )
-mean(dailyreturns_exp)
-sd(dailyreturns_exp)
-hist(dailyreturns_exp)
-
-# Generating Daily Price from Returns 
-stockPrice <- 0.06
-r <-dailyreturns_exp
-stock_prices_exp <- c()
-for (i in seq(tradingdays))
-{
-  stockPrice <- stockPrice*(1+r[i])
-  stock_prices_exp <- c(stock_prices_exp,stockPrice)
-  
+portfolio_returns = NULL
+for (i in seq(simulations)) {
+  stockPrices = scenario_stock_generation(tradingdays, scenario)
+  print(stockPrices)
+  stockReturns = na.omit(ROC(stockPrices, type="discrete"))
+  stockReturns = as.timeSeries(stockReturns)
+  meanReturns=colMeans(stockReturns)
+  covariancematrix=cov(stockReturns)*tradingdays
+  print(covariancematrix)
+  meanvar=optimalPortfolio(covariancematrix,meanReturns, control=list(type='mv',constraint='lo'))
+  minvar=optimalPortfolio(covariancematrix,meanReturns, control=list(type='minvol',constraint='lo'))
+  maxdiv=optimalPortfolio(covariancematrix,meanReturns, control=list(type='maxdiv',constraint='lo'))
+  maxdec=optimalPortfolio(covariancematrix,meanReturns, control=list(type='maxdec',constraint='lo'))    
+  portfolio_returns = c(portfolio_returns, maxdec)
 }
-print(stock_prices_exp)
---
+++ Simulation.R
@@ -0,0 +1,75 @@
+library(RiskPortfolios)
+library(timeSeries)
+library(fPortfolio)
+library(caTools)
+library(dplyr)
+library(PerformanceAnalytics)
+#Distribution generation of stock returns for a specified number of days 
+#and type(normal, lognormal, exponential)
+generate_distribution <- function(number, type) {
+  tradingdays<-number
+  m <- runif(1, min= 0.001, max = 0.05)
+  s <- runif(1, min=0.000001, max = 0.05)
+  lambda = runif(1, min=25, max=50)
+  dailyreturns = c()
+  minimumPrice = 0.001
+  maximumPrice = 15
+  if(type == "normal") {
+    dailyreturns <-rnorm(n=tradingdays,m,s)
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else if(type == "lognormal") {
+    meanlog<-log(m^2/sqrt(s^2+m^2))
+    stdlog<- sqrt(log(1+(s^2/m^2)))
+    #Vector of daily returns 
+    dailyreturns<- rlnorm(n=tradingdays,meanlog,stdlog)
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else if(type == "exponential") {
+    maximumPrice = 0.15
+    dailyreturns = rexp(n=tradingdays, rate = lambda )
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else {
+    stop("No specified distribution")
+  }
+  # Generating Daily Price from Returns 
+  STK_PRC <- runif(1, min = minimumPrice, max = maximumPrice)
+  r <-dailyreturns
+  for (i in seq(tradingdays))
+  {
+    STK_PRC <- STK_PRC*(1+r[i])
+    stock_prices<- c(stock_prices,STK_PRC)
+    
+  }
+  return(stock_prices)
+}
+
+#Stock distribution generation based on scenarios
+scenario_stock_generation <- function(tradingdays, scenario) {
+  tickers = c("A", "B", "C", "D", "E")
+  stockPrices = NULL
+  if(scenario == "allnormal") {
+    for (ticker in tickers) {
+      stockPrices = cbind(stockPrices,
+                          generate_distribution(tradingdays, "normal"))
+    }
+  } else if(scenario == "mix") {
+    for (ticker in tickers) {
+      if(ticker == "B") {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "exponential"))
+      } else if(ticker == "C") {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "lognormal"))
+      } else {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "normal"))
+      }
+    }
+  } else {
+    stop("No specified scenario")
+  }
+  colnames(stockPrices) = tickers
+  return(stockPrices)
+}
