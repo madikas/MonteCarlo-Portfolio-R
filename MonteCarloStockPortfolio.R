b4affd53b297453b1df403a5e97ecc9ec9aa701a
kasmadius <60152263+kasmadius@users.noreply.github.com>
2020-11-10 20:13:15 +0600

    Final Version (#2)
    * Exponential distribution generation
    
    * simulation run scenarios
    
    * fix(generate_distribution)
    
    * mix and allnormal scenarios implemented
    
    * variance, return and sharperatio calculations for portfolio strategies
    
    * all scenarios complete for portfolio strategies
    
    * equal weights portfolio simulation added
    
    * seed added for reproducibility + report markdown
    
    * Update MonteCarloStockPortfolio.R
    
    Added density plots for simulations, and tables for stats
    
    * final version
    
    Co-authored-by: Madi Kas <kas-madius@gmail.com>
    Co-authored-by: stephanieevu <73712770+stephanieevu@users.noreply.github.com>

--- MonteCarloStockPortfolio.R
+++ MonteCarloStockPortfolio.R
@@ -1,42 +1,311 @@
-library(quantmod) 
-library(timeSeries)
-library(fPortfolio)
-library(caTools)
-library(dplyr)
-library(PerformanceAnalytics)
-library(ggplot2)
+source("Simulation.R")
+#Plots for portfolio Returns 
+library(ggpubr)
+library(kableExtra)
+library(DescTools)
+#MonteCarlo simulation of portfolio strategies 
+#given different parameters(combination of distributions)
+simulations = 1000
+tradingdays = 253
+scenario = "allnormal"
+set.seed(123)
+portfolio_returns = portfolio_simulation(simulations, tradingdays, scenario)
+portfolio_returns
+allnorm_ret=na.omit(portfolio_returns)
+allnorm_mean=round(colMeans(na.omit(portfolio_returns)),6)
 
-#Vector of Stock Tickers
-tickers=c("GOOGL", "AMZN", "PFE", "JPM", "CVX", "BA", "FAST", "INTC", 
-          "T", "NWSA")
+scenario = "mix"
+portfolio_returns = portfolio_simulation(simulations, tradingdays, scenario)
+portfolio_returns
+mix_ret=na.omit(portfolio_returns)
+mix_ret_mean=round(colMeans(na.omit(portfolio_returns)),6)
 
-#Daily Prices for the last five years
-stockPrices = NULL
-for (ticker in tickers)
-  stockPrices = cbind(stockPrices,
-                      getSymbols.yahoo(ticker, from="2015-01-01",
-                                       periodicity = "daily",
-                                        auto.assign=FALSE)[,4])
-#Keep dates that have closing prices for all tickers
-stockPrices = stockPrices[apply(stockPrices, 1, function(x) all(!is.na(x))),]
-#Rename Columns matching stock ticker names
-colnames(stockPrices) = tickers
+scenario = "low volatility"
+portfolio_returns = portfolio_simulation(simulations, tradingdays, scenario)
+portfolio_returns
+lowvol_ret=na.omit(portfolio_returns)
+lowvol_mean=round(colMeans(na.omit(portfolio_returns)),6)
 
-#Daily Rate of Change
-stockReturns = na.omit(ROC(stockPrices, type="discrete"))
-stockReturns = as.timeSeries(stockReturns)
+scenario = "high volatility"
+portfolio_returns = portfolio_simulation(simulations, tradingdays, scenario)
+portfolio_returns
+highvol_ret=na.omit(portfolio_returns)
+highvol_mean=round(colMeans(na.omit(portfolio_returns)),6)
 
-#efficient frontier
-effFrontier = portfolioFrontier(stockReturns, constraints = "LongOnly")
 
-#Plot efficient frontier
-#Plot options fportfolio
-#1 - Plot efficient frontier
-#2 - Plot Min Variance Portfolio
-#3 - Plot Tangency Portfolio
-#4 - Plot Risk Returns of each ticker
-#5 - Plot Equal Weights Portfolio
-#6 - Plot Two Asset Frontiers
-#7 - Plot Monte Carlo Portfolios
-#8 - Plot Sharpe Ratio
-plot(effFrontier, 1)
+#All Normal
+  a1<- ggplot(allnorm_ret, aes(x=meanvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(meanvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Mean Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  a2<- ggplot(allnorm_ret, aes(x=minvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(minvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Min Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  a3<- ggplot(allnorm_ret, aes(x=maxdivReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdivReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Diversification",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  a4<-ggplot(allnorm_ret, aes(x=maxdecReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdecReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Decorrelation",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  a5<-ggplot(allnorm_ret, aes(x=equalweightsReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(equalweightsReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Equal Weights",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  
+  figure1<-ggarrange(a1,a2,a3,a4,a5,
+                     ncol=3,nrow=2)
+  
+  annotate_figure(figure1,
+                  top = text_grob("Portfolio of All Normal Stocks", color = "black", face = "bold", size = 14),
+                  fig.lab = "Figure 1", fig.lab.face = "bold")
+ 
+
+#Mixed
+  b1<- ggplot(mix_ret, aes(x=meanvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(meanvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Mean Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  b2<- ggplot(mix_ret, aes(x=minvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(minvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Min Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  b3<- ggplot(mix_ret, aes(x=maxdivReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdivReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Diversification",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  b4<-ggplot(mix_ret, aes(x=maxdecReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdecReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Decorrelation",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  b5<-ggplot(mix_ret, aes(x=equalweightsReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(equalweightsReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Equal Weights",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  
+  figure2<-ggarrange(b1,b2,b3,b4,b5,
+                     ncol=3,nrow=2)
+  
+  annotate_figure(figure2,
+                  top = text_grob("Portfolio of Mixed Distribution Stocks", color = "black", face = "bold", size = 14),
+                  fig.lab = "Figure 2", fig.lab.face = "bold")
+
+
+#Low Volatility
+  
+  c1<- ggplot(lowvol_ret, aes(x=meanvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(meanvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Mean Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  c2<- ggplot(lowvol_ret, aes(x=minvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(minvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Min Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  c3<- ggplot(lowvol_ret, aes(x=maxdivReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdivReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Diversification",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  c4<-ggplot(lowvol_ret, aes(x=maxdecReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdecReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Decorrelation",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  c5<-ggplot(lowvol_ret, aes(x=equalweightsReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(equalweightsReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Equal Weights",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  
+  figure3<-ggarrange(c1,c2,c3,c4,c5,
+                     ncol=3,nrow=2)
+  
+  annotate_figure(figure3,
+                  top = text_grob("Portfolio of Low Volatility Stocks", color = "black", face = "bold", size = 14),
+                  fig.lab = "Figure 3", fig.lab.face = "bold")
+  
+
+#High volatility
+
+  d1<- ggplot(highvol_ret, aes(x=meanvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(meanvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Mean Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  d2<- ggplot(highvol_ret, aes(x=minvarReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(minvarReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Min Variance",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  d3<- ggplot(highvol_ret, aes(x=maxdivReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdivReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Diversification",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  d4<-ggplot(highvol_ret, aes(x=maxdecReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(maxdecReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Max Decorrelation",x="Return", y = "Frequency")+
+    theme_classic()
+  
+  d5<-ggplot(highvol_ret, aes(x=equalweightsReturn)) +
+    geom_histogram(fill="white", color="black")+
+    geom_vline(aes(xintercept=mean(equalweightsReturn,na.rm=TRUE)), color="blue",
+               linetype="dashed")+
+    labs(title="Equal Weights",x="Return", y = "Frequency")+
+    theme_classic()
+
+
+figure4<-ggarrange(d1,d2,d3,d4,d5,
+                   ncol=3,nrow=2)
+
+annotate_figure(figure4,
+                top = text_grob("Portfolio of High Volatility Stocks", color = "black", face = "bold", size = 14),
+                fig.lab = "Figure 4 ", fig.lab.face = "bold")
+
+#Confidence Intervals
+
+  #All Normal
+  
+  allnorm_CI=MeanCI(allnorm_ret$meanvarReturn, conf.level = 0.95,na.rm=TRUE)
+  allnorm_CI=rbind(allnorm_CI,MeanCI(allnorm_ret$minvarReturn, conf.level = 0.95,na.rm=TRUE),
+ MeanCI(allnorm_ret$maxdivReturn, conf.level = 0.95,na.rm=TRUE),
+ MeanCI(allnorm_ret$maxdecReturn, conf.level = 0.95,na.rm=TRUE),
+ MeanCI(allnorm_ret$equalweightsReturn, conf.level = 0.95,na.rm=TRUE))
+  allnorm_CI=round(allnorm_CI,6)
+
+  #Mixed
+  mix_CI=MeanCI(mix_ret$meanvarReturn, conf.level = 0.95,na.rm=TRUE)
+  mix_CI=rbind(mix_CI,MeanCI(mix_ret$minvarReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(mix_ret$maxdivReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(mix_ret$maxdecReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(mix_ret$equalweightsReturn, conf.level = 0.95,na.rm=TRUE))
+  mix_CI=round(mix_CI,6)
+  
+  #Low Volatility
+  low_CI=MeanCI(lowvol_ret$meanvarReturn, conf.level = 0.95,na.rm=TRUE)
+  low_CI=rbind(low_CI,MeanCI(lowvol_ret$minvarReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(lowvol_ret$maxdivReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(lowvol_ret$maxdecReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(lowvol_ret$equalweightsReturn, conf.level = 0.95,na.rm=TRUE))
+  low_CI=round(low_CI,6)
+  
+  #High Volatility
+  high_CI=MeanCI(highvol_ret$meanvarReturn, conf.level = 0.95,na.rm=TRUE)
+  high_CI=rbind(high_CI,MeanCI(highvol_ret$minvarReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(highvol_ret$maxdivReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(highvol_ret$maxdecReturn, conf.level = 0.95,na.rm=TRUE),
+  MeanCI(highvol_ret$equalweightsReturn, conf.level = 0.95,na.rm=TRUE))
+  high_CI=round(high_CI,6)
+  
+#Tables 
+  
+  #All norm
+  t1=data.frame(rbind(allnorm_mean[1:3],allnorm_mean[4:6],allnorm_mean[7:9],allnorm_mean[10:12],allnorm_mean[13:15]))
+  t1=cbind(t1,allnorm_CI[,2:3])
+  rownames(t1)<-c("Mean Variance","Min Variance","Max Diversification","Max Decorrelation","Equal Weights")
+  colnames(t1)<- c("Return (%)","Variance (%)","Sharpe", "Lower CI","Upper CI")
+  t1[1:2]<-t1[1:2]*100
+  t1[4:5]<-t1[4:5]*100
+  t1
+  
+  
+  t1 %>%
+    kbl(caption = "Table 1: Portfolio of All Normal Stocks") %>%
+    kable_classic(full_width = F, html_font = "Cambria")
+  
+  #Mixed
+  t2=data.frame(rbind(mix_ret_mean[1:3],mix_ret_mean[4:6],mix_ret_mean[7:9],mix_ret_mean[10:12],mix_ret_mean[13:15]))
+  t2=cbind(t2,mix_CI[,2:3])
+  rownames(t2)<-c("Mean Variance","Min Variance","Max Diversification","Max Decorrelation","Equal Weights")
+  colnames(t2)<- c("Return (%)","Variance (%)","Sharpe", "Lower CI","Upper CI")
+  t2[1:2]<-t2[1:2]*100
+  t2[4:5]<-t2[4:5]*100
+  t2
+  
+  t2 %>%
+    kbl(caption = "Table 2: Portfolio of Mixed Distribution Stocks") %>%
+    kable_classic(full_width = F, html_font = "Cambria")
+  
+  
+  #Low Volatility
+ t3=data.frame(rbind(lowvol_mean[1:3],lowvol_mean[4:6],lowvol_mean[7:9],lowvol_mean[10:12],lowvol_mean[13:15]))
+ t3=cbind(t3,low_CI[,2:3])
+  rownames(t3)<-c("Mean Variance","Min Variance","Max Diversification","Max Decorrelation","Equal Weights")
+  colnames(t3)<- c("Return (%)","Variance (%)","Sharpe", "Lower CI","Upper CI")
+  t3[1:2]<-t3[1:2]*100
+  t3[4:5]<-t3[4:5]*100
+  t3
+ 
+ t3 %>%
+   kbl(caption = "Table 3: Portfolio of Low Volatility Stocks") %>%
+   kable_classic(full_width = F, html_font = "Cambria")
+ 
+ 
+ #High Volatility
+ t4=data.frame(rbind(highvol_mean[1:3],highvol_mean[4:6],highvol_mean[7:9],highvol_mean[10:12],highvol_mean[13:15]))
+ t4=cbind(t4,high_CI[,2:3])
+ rownames(t4)<-c("Mean Variance","Min Variance","Max Diversification","Max Decorrelation","Equal Weights")
+ colnames(t4)<- c("Return (%)","Variance (%)","Sharpe", "Lower CI","Upper CI")
+ t4[1:2]<-t4[1:2]*100
+ t4[4:5]<-t4[4:5]*100
+ t4
+ 
+ t4 %>%
+   kbl(caption = "Table 4: Portfolio of High Volatility Stocks") %>%
+   kable_classic(full_width = F, html_font = "Cambria")
+ 
+ 
+ 
+ 
---
+++ Simulation.R
@@ -0,0 +1,164 @@
+library(ggplot2)
+library(RiskPortfolios)
+library(timeSeries)
+library(fPortfolio)
+library(caTools)
+library(dplyr)
+library(PerformanceAnalytics)
+library(TTR)
+# Local functions
+cbrt <- function (x) {
+  sign(x) * abs(x)^(1/3)
+}
+#U-quadratic shape distributed returns (with paramets a = -b) for 253 days
+ruquad <- function(upper_bound , nsim) {
+  b <- upper_bound
+  a <- -b
+  Beta <- (b+a)/2
+  Alpha <- 12/((b-a)^3)
+  unif <- runif(nsim , min = 0 , max = 1)
+  df_unif <- as.data.frame(unif)
+  df_uquad <- within(df_unif ,x <- (((3*unif)/Alpha)-((Beta-a)^3)))
+  df_uquad <- apply(df_uquad , 2 , cbrt)
+  uquad_ret <- df_uquad[,2]
+  return(uquad_ret)
+}
+#Distribution generation of stock returns for a specified number of days 
+#and type(normal, lognormal, exponential)
+generate_distribution <- function(number, type, volatility) {
+  tradingdays<-number
+  m <- runif(1, min= 0.001, max = 0.05)
+  if(volatility == "high") {
+    s <- runif(1, min=0.5, max = 1)
+  } else if(volatility == "low") {
+    s <- runif(1, min=0.000001, max = 0.01)
+  } else {
+    s <- runif(1, min=0.000001, max = 0.05)
+  }
+  up <- runif(1 , min=0.03 , max = 0.1)
+  lambda = runif(1, min=25, max=50)
+  dailyreturns = c()
+  minimumPrice = 0.001
+  maximumPrice = 15
+  if(type == "normal") {
+    dailyreturns <-rnorm(n=tradingdays,m,s)
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else if(type == "lognormal") {
+    meanlog<-log(m^2/sqrt(s^2+m^2))
+    stdlog<- sqrt(log(1+(s^2/m^2)))
+    #Vector of daily returns 
+    dailyreturns<- rlnorm(n=tradingdays,meanlog,stdlog)
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else if(type == "exponential") {
+    maximumPrice = 0.15
+    dailyreturns = rexp(n=tradingdays, rate = lambda )
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else if(type == "uquad") {
+    dailyreturns = ruquad(up ,tradingdays)
+    mean(dailyreturns)
+    sd(dailyreturns)
+  } else {
+    stop("No specified distribution")
+  }
+  # Generating Daily Price from Returns 
+  STK_PRC <- runif(1, min = minimumPrice, max = maximumPrice)
+  r <-dailyreturns
+  stock_prices = c()
+  for (i in seq(tradingdays))
+  {
+    STK_PRC <- STK_PRC*(1+r[i])
+    stock_prices<- c(stock_prices,STK_PRC)
+    
+  }
+  return(stock_prices)
+}
+
+#Stock distribution generation based on scenarios
+scenario_stock_generation <- function(tradingdays, scenario) {
+  tickers = c("A", "B", "C", "D", "E")
+  stockPrices = NULL
+  if(scenario == "allnormal") {
+    for (ticker in tickers) {
+      stockPrices = cbind(stockPrices,
+                          generate_distribution(tradingdays, "normal",""))
+    }
+  } else if(scenario == "mix") {
+    for (ticker in tickers) {
+      if(ticker == "B") {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "exponential",""))
+      } else if(ticker == "C") {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "lognormal",""))
+      } else if(ticker == "D") {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "uquad",""))
+      } else {
+        stockPrices = cbind(stockPrices,
+                            generate_distribution(tradingdays, "normal",""))
+      }
+    }
+  } else if(scenario=="low volatility") {
+    for (ticker in tickers) {
+      stockPrices = cbind(stockPrices,
+                          generate_distribution(tradingdays, "normal", "low"))
+    }
+  } else if(scenario=="high volatility") {
+    for (ticker in tickers) {
+      stockPrices = cbind(stockPrices,
+                          generate_distribution(tradingdays, "normal", "high"))
+    }
+  }else {
+    stop("No specified scenario")
+  }
+  colnames(stockPrices) = tickers
+  return(stockPrices)
+}
+
+#Rate of change calculation
+D_ret = function(x) na.omit(ROC(x, type="discrete"))
+#Monte Carlo Simulation for portfolios based on tradingdays, numsimulations and
+#scenarios
+portfolio_simulation <- function(simulations,tradingdays, scenario) {
+  portfolio_returns = data.frame(meanvarReturn=NA, meanvarVariance=NA, meanvarSharpe=NA,  
+                                 minvarReturn= NA, minvarVariance=NA, minvarSharpe=NA,
+                                 maxdivReturn=NA, maxdivVariance=NA, maxdivSharpe=NA,
+                                 maxdecReturn=NA, maxdecVariance=NA, maxdecSharpe=NA,
+                                 equalweightsReturn=NA, equalweightsVariance=NA, equalweightsSharpe=NA)
+  newrow = NULL
+  for (i in seq(simulations)) {
+    stockPrices = scenario_stock_generation(tradingdays, scenario)
+    stockReturns = apply(stockPrices, 2, D_ret)
+    stockReturns = as.timeSeries(stockReturns)
+    meanReturns=as.matrix(colMeans(stockReturns))
+    covariancematrix=as.matrix(cov(stockReturns)*tradingdays)
+    meanvar=as.matrix(optimalPortfolio(covariancematrix,meanReturns, control=list(type='mv',constraint='lo')))
+    minvar=as.matrix(optimalPortfolio(covariancematrix,meanReturns, control=list(type='minvol',constraint='lo')))
+    maxdiv=as.matrix(optimalPortfolio(covariancematrix,meanReturns, control=list(type='maxdiv',constraint='lo')))
+    maxdec=as.matrix(optimalPortfolio(covariancematrix,meanReturns, control=list(type='maxdec',constraint='lo')))
+    equalweights = as.matrix(c(0.2,0.2,0.2,0.2,0.2))
+    meanvarReturn =  t(meanvar) %*% meanReturns
+    meanvarVariance =  t(meanvar) %*% covariancematrix %*% meanReturns
+    meanvarSharpe = meanvarReturn / sqrt(meanvarVariance)
+    minvarReturn =  t(minvar) %*% meanReturns
+    minvarVariance =  t(minvar) %*%  covariancematrix %*% meanReturns
+    minvarSharpe = minvarReturn / sqrt(minvarVariance)
+    maxdivReturn =  t(maxdiv) %*% meanReturns
+    maxdivVariance =  t(maxdiv) %*%  covariancematrix %*% meanReturns
+    maxdivSharpe = maxdivReturn / sqrt(maxdivVariance)
+    maxdecReturn =  t(maxdec) %*% meanReturns
+    maxdecVariance =  t(maxdec) %*%  covariancematrix %*% meanReturns
+    maxdecSharpe = maxdecReturn / sqrt(maxdecVariance)
+    equalweightsReturn =  t(equalweights) %*% meanReturns
+    equalweightsVariance =  t(equalweights) %*%  covariancematrix %*% meanReturns
+    equalweightsSharpe = equalweightsReturn / sqrt(equalweightsVariance)
+    newrow = c(meanvarReturn,meanvarVariance, meanvarSharpe, minvarReturn, minvarVariance, minvarSharpe,
+               maxdivReturn,maxdivVariance, maxdivSharpe, maxdecReturn, maxdecVariance, maxdecSharpe,
+               equalweightsReturn, equalweightsVariance, equalweightsSharpe)
+    portfolio_returns = rbind(portfolio_returns[1:i,],newrow,portfolio_returns[-(1:i),])
+  }
+  return(portfolio_returns)
+}
